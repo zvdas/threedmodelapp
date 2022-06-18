271f4678ea00f15d377978fc965691c96e2893c3
zvdas <rodriguezzude@gmail.com>
2022-06-18 20:57:48 +0530

    full stack deploy

--- nodejsapp/index.js
+++ nodejsapp/index.js
@@ -12,7 +12,9 @@ const app = express();
 
 app.use(express.static(path));
 
-app.use(cors({ origin: 'http://localhost:4200' }));
+// app.use(cors({ origin: 'http://localhost:4200' }));
+
+app.use(cors({ origin: 'https://threedmodelapp.herokuapp.com/threed/api' }));
 
 // parse requests of content-type - application/json
 app.use(express.json({ limit: '50mb' }));
